7227081dd4d54f785cd525dd0bc48efdb6c610df
Aaditya Rasala <aadityarasala19@gnu.ac.in>
2022-05-01 11:55:06 +0530

    developing navigation

--- app/src/main/java/com/example/myapplication/home.java
+++ app/src/main/java/com/example/myapplication/home.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -31,6 +33,11 @@ public class home extends AppCompatActivity {
         // Find our drawer view
         mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
 
+        // Find our drawer view
+        nvDrawer = (NavigationView) findViewById(R.id.nvView);
+        // Setup drawer view
+        setupDrawerContent(nvDrawer);
+
     }
 
     @Override
@@ -43,5 +50,73 @@ public class home extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private void setupDrawerContent(NavigationView navigationView) {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    public void selectDrawerItem(MenuItem menuItem) {
+        // Create a new fragment and specify the fragment to show based on nav item clicked
+        Fragment fragment = null;
+        Class fragmentClass;
+        switch(menuItem.getItemId()) {
+            case R.id.nav_1_fragment:
+                fragmentClass = home.class;
+                break;
+//            case R.id.nav_2_fragment:
+//                fragmentClass = "page nu nam".class;
+//                break;
+//            case R.id.nav_3_fragment:
+//                fragmentClass = "page nu nam".class;
+//                break;
+//            case R.id.nav_4_fragment:
+//                fragmentClass = "page nu nam".class;
+//                break;
+//            case R.id.nav_5_fragment:
+//                fragmentClass = "page nu nam".class;
+//                break;
+//            case R.id.nav_6_fragment:
+//                fragmentClass = "page nu nam".class;
+//                break;
+//            case R.id.nav_7_fragment:
+//                fragmentClass = "page nu nam".class;
+//                break;
+//            case R.id.nav_8_fragment:
+//                fragmentClass = "page nu nam".class;
+//                break;
+//            case R.id.nav_9_fragment:
+//                fragmentClass = "page nu nam".class;
+//                break;
+//            case R.id.nav_10_fragment:
+//                fragmentClass = "page nu nam".class;
+//                break;
+            default:
+                fragmentClass = home.class;
+        }
+
+        try {
+            fragment = (Fragment) fragmentClass.newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // Insert the fragment by replacing any existing fragment
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+
+        // Highlight the selected item has been done by NavigationView
+        menuItem.setChecked(true);
+        // Set action bar title
+        setTitle(menuItem.getTitle());
+        // Close the navigation drawer
+        mDrawer.closeDrawers();
     }
 }
