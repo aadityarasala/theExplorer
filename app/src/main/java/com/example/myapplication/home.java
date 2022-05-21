e6c035cf51a9a741ee627da57c7c8249828e6f10
Aaditya Rasala <aadityarasala19@gnu.ac.in>
2022-05-21 10:06:27 +0530

    discover_beauty1

--- app/src/main/java/com/example/myapplication/home.java
+++ app/src/main/java/com/example/myapplication/home.java
@@ -19,151 +19,151 @@ import android.widget.ImageView;
 import com.google.android.material.navigation.NavigationView;
 
 public class home extends AppCompatActivity {
-    private DrawerLayout mDrawer;
-    private Toolbar toolbar;
-    private NavigationView nvDrawer;
-
-    // Make sure to be using androidx.appcompat.app.ActionBarDrawerToggle version.
-    private ActionBarDrawerToggle drawerToggle;
-    //private ActionBarDrawerToggle drawerToogle;
+//    private DrawerLayout mDrawer;
+//    private Toolbar toolbar;
+//    private NavigationView nvDrawer;
+//
+//    // Make sure to be using androidx.appcompat.app.ActionBarDrawerToggle version.
+//    private ActionBarDrawerToggle drawerToggle;
+//    //private ActionBarDrawerToggle drawerToogle;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-
-        // Set a Toolbar to replace the ActionBar.
-        toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        // This will display an Up icon (<-), we will replace it with hamburger later
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-
-        // Find our drawer view
-        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawerToggle = setupDrawerToggle();
-
-        // Find our drawer view
-        nvDrawer = (NavigationView) findViewById(R.id.nvView);
-        // Setup drawer view
-        setupDrawerContent(nvDrawer);
-
-
-        // Setup toggle to display hamburger icon with nice animation
-        //drawerToogle.setDrawerIndicatorEnabled(true);
-        drawerToggle.syncState();
-
-        // Tie DrawerLayout events to the ActionBarToggle
-        mDrawer.addDrawerListener(drawerToggle);
-
-
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (drawerToggle.onOptionsItemSelected(item)) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-        // The action bar home/up action should open or close the drawer.
-//        switch (item.getItemId()) {
-//            case android.R.id.home:
-//                mDrawer.openDrawer(GravityCompat.START);
-//                return true;
+//
+//        // Set a Toolbar to replace the ActionBar.
+//        toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+//
+//        // This will display an Up icon (<-), we will replace it with hamburger later
+//        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+//
+//        // Find our drawer view
+////        mDrawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+////        drawerToggle = setupDrawerToggle();
+////
+////        // Find our drawer view
+////        nvDrawer = (NavigationView) findViewById(R.id.nvView);
+////        // Setup drawer view
+////        setupDrawerContent(nvDrawer);
+//
+//
+//        // Setup toggle to display hamburger icon with nice animation
+//        //drawerToogle.setDrawerIndicatorEnabled(true);
+//        drawerToggle.syncState();
+//
+//        // Tie DrawerLayout events to the ActionBarToggle
+//        mDrawer.addDrawerListener(drawerToggle);
+//
+//
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        if (drawerToggle.onOptionsItemSelected(item)) {
+//            return true;
 //        }
 //        return super.onOptionsItemSelected(item);
-    }
-
-    private void setupDrawerContent(NavigationView navigationView) {
-        navigationView.setNavigationItemSelectedListener(
-                new NavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(MenuItem menuItem) {
-                        selectDrawerItem(menuItem);
-                        return true;
-                    }
-                });
-    }
-
-    public void selectDrawerItem(MenuItem menuItem) {
-        // Create a new fragment and specify the fragment to show based on nav item clicked
-        Fragment fragment = null;
-        Class fragmentClass;
-        switch(menuItem.getItemId()) {
-            case R.id.nav_1_fragment:
-                fragmentClass = home.class;
-                break;
-//            case R.id.nav_2_fragment:
-//                fragmentClass = "page nu nam".class;
+//        // The action bar home/up action should open or close the drawer.
+////        switch (item.getItemId()) {
+////            case android.R.id.home:
+////                mDrawer.openDrawer(GravityCompat.START);
+////                return true;
+////        }
+////        return super.onOptionsItemSelected(item);
+//    }
+//
+//    private void setupDrawerContent(NavigationView navigationView) {
+//        navigationView.setNavigationItemSelectedListener(
+//                new NavigationView.OnNavigationItemSelectedListener() {
+//                    @Override
+//                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+//                        selectDrawerItem(menuItem);
+//                        return true;
+//                    }
+//                });
+//    }
+//
+//    public void selectDrawerItem(MenuItem menuItem) {
+//        // Create a new fragment and specify the fragment to show based on nav item clicked
+//        Fragment fragment = null;
+//        Class fragmentClass;
+//        switch(menuItem.getItemId()) {
+//            case R.id.nav_1_fragment:
+//                fragmentClass = home.class;
 //                break;
-//            case R.id.nav_3_fragment:
-//                fragmentClass = "page nu nam".class;
-//                break;
-//            case R.id.nav_4_fragment:
-//                fragmentClass = "page nu nam".class;
-//                break;
-//            case R.id.nav_5_fragment:
-//                fragmentClass = "page nu nam".class;
-//                break;
-//            case R.id.nav_6_fragment:
-//                fragmentClass = "page nu nam".class;
-//                break;
-//            case R.id.nav_7_fragment:
-//                fragmentClass = "page nu nam".class;
-//                break;
-//            case R.id.nav_8_fragment:
-//                fragmentClass = "page nu nam".class;
-//                break;
-//            case R.id.nav_9_fragment:
-//                fragmentClass = "page nu nam".class;
-//                break;
-//            case R.id.nav_10_fragment:
-//                fragmentClass = "page nu nam".class;
-//                break;
-            default:
-                fragmentClass = home.class;
-        }
-
-        try {
-            fragment = (Fragment) fragmentClass.newInstance();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        // Insert the fragment by replacing any existing fragment
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
-
-        // Highlight the selected item has been done by NavigationView
-        menuItem.setChecked(true);
-        // Set action bar title
-        setTitle(menuItem.getTitle());
-        // Close the navigation drawer
-        mDrawer.closeDrawers();
-    }
-
-    private ActionBarDrawerToggle setupDrawerToggle() {
-        // NOTE: Make sure you pass in a valid toolbar reference.  ActionBarDrawToggle() does not require it
-        // and will not render the hamburger icon without it.
-        return new ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.drawer_open,  R.string.drawer_close);
-    }
-
-    // `onPostCreate` called when activity start-up is complete after `onStart()`
-    // NOTE 1: Make sure to override the method with only a single `Bundle` argument
-    // Note 2: Make sure you implement the correct `onPostCreate(Bundle savedInstanceState)` method.
-    // There are 2 signatures and only `onPostCreate(Bundle state)` shows the hamburger icon.
-    @Override
-    protected void onPostCreate(Bundle savedInstanceState) {
-        super.onPostCreate(savedInstanceState);
-        // Sync the toggle state after onRestoreInstanceState has occurred.
-        drawerToggle.syncState();
-    }
-
-    @Override
-    public void onConfigurationChanged(Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        // Pass any configuration change to the drawer toggles
-        drawerToggle.onConfigurationChanged(newConfig);
+////            case R.id.nav_2_fragment:
+////                fragmentClass = "page nu nam".class;
+////                break;
+////            case R.id.nav_3_fragment:
+////                fragmentClass = "page nu nam".class;
+////                break;
+////            case R.id.nav_4_fragment:
+////                fragmentClass = "page nu nam".class;
+////                break;
+////            case R.id.nav_5_fragment:
+////                fragmentClass = "page nu nam".class;
+////                break;
+////            case R.id.nav_6_fragment:
+////                fragmentClass = "page nu nam".class;
+////                break;
+////            case R.id.nav_7_fragment:
+////                fragmentClass = "page nu nam".class;
+////                break;
+////            case R.id.nav_8_fragment:
+////                fragmentClass = "page nu nam".class;
+////                break;
+////            case R.id.nav_9_fragment:
+////                fragmentClass = "page nu nam".class;
+////                break;
+////            case R.id.nav_10_fragment:
+////                fragmentClass = "page nu nam".class;
+////                break;
+//            default:
+//                fragmentClass = home.class;
+//        }
+//
+//        try {
+//            fragment = (Fragment) fragmentClass.newInstance();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+//
+////        // Insert the fragment by replacing any existing fragment
+////        FragmentManager fragmentManager = getSupportFragmentManager();
+////        fragmentManager.beginTransaction().replace(R.id.flContent, fragment).commit();
+////
+////        // Highlight the selected item has been done by NavigationView
+////        menuItem.setChecked(true);
+////        // Set action bar title
+////        setTitle(menuItem.getTitle());
+////        // Close the navigation drawer
+////        mDrawer.closeDrawers();
+//    }
+//
+//    private ActionBarDrawerToggle setupDrawerToggle() {
+//        // NOTE: Make sure you pass in a valid toolbar reference.  ActionBarDrawToggle() does not require it
+//        // and will not render the hamburger icon without it.
+//        return new ActionBarDrawerToggle(this, mDrawer, toolbar, R.string.drawer_open,  R.string.drawer_close);
+//    }
+//
+//    // `onPostCreate` called when activity start-up is complete after `onStart()`
+//    // NOTE 1: Make sure to override the method with only a single `Bundle` argument
+//    // Note 2: Make sure you implement the correct `onPostCreate(Bundle savedInstanceState)` method.
+//    // There are 2 signatures and only `onPostCreate(Bundle state)` shows the hamburger icon.
+//    @Override
+//    protected void onPostCreate(Bundle savedInstanceState) {
+//        super.onPostCreate(savedInstanceState);
+//        // Sync the toggle state after onRestoreInstanceState has occurred.
+//        drawerToggle.syncState();
+//    }
+//
+//    @Override
+//    public void onConfigurationChanged(Configuration newConfig) {
+//        super.onConfigurationChanged(newConfig);
+//        // Pass any configuration change to the drawer toggles
+//        drawerToggle.onConfigurationChanged(newConfig);
     }
 }
